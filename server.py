d441be80d06ede00401ad4bb23d17137afc5c46d
David Snyder <davidsnyder@loki.local>
2018-03-10 19:03:46 +0900

    server script can serve html files

--- server.py
+++ server.py
@@ -1,19 +1,23 @@
 import socket
+import os
 
 HOST = "127.0.0.1"
 PORT = 9000
-
-# RESPONSE_BYTE = b"""\
-# HTTP/1.1 200 OK
-# Content-type: text/html
-# Content-length: 15
-
-# <h1>Hello!</h1>"""
+SERVER_ROOT = os.path.abspath("www")
 
 HTML_BODY_200 = "<h1>Hello!</h1>"
 HTML_BODY_400 = "Bad Request"
+HTML_BODY_404 = "Not Found"
+HTML_BODY_405 = "Method Not Allowed"
 
-RESPONSE = f"""\
+FILE_HEADERS = """\
+HTTP/1.1 200 OK
+Content-type: {content_type}
+Content-length: {content_length}
+
+"""
+
+OK_RESPONSE = f"""\
 HTTP/1.1 200 OK
 Content-type: text/html
 Content-length: {len(HTML_BODY_200)}
@@ -22,10 +26,24 @@ Content-length: {len(HTML_BODY_200)}
 
 BAD_REQUEST_RESPONSE = f"""\
 HTTP/1.1 400 Bad Request
-Content-type: text/html
+Content-type: text/plain
 Content-length: {len(HTML_BODY_400)}
 
 {HTML_BODY_400}""".encode('ASCII')
+
+NOT_FOUND_RESPONSE = f"""\
+HTTP/1.1 404 Not Found
+Content-type: text/plain
+Content-length: {len(HTML_BODY_404)}
+
+{HTML_BODY_404}""".encode('ASCII')
+
+METHOD_NOT_ALLOWED_RESPONSE = f"""\
+HTTP/1.1 405 Method Not Allowed
+Content-type: text/plain
+Content-length: {len(HTML_BODY_405)}
+
+{HTML_BODY_405}""".encode('ASCII')
 
 with socket.socket() as server_sock:
 	#SO_REUSEADDR reuses sockets in a TIME_WAIT state, without waiting for the timeout to expire
@@ -51,9 +69,24 @@ with socket.socket() as server_sock:
 					else:
 						name, _, value = line.partition(":")
 						headers[name.lower()] = value.lstrip()
-				print({"method": method, "path": path, "headers": headers})
-				client_sock.sendall(RESPONSE)
-				client_sock.close()
+				request = {"method": method, "path": path, "headers": headers}
+				print(request)
+				if request['method'] != "GET":
+					client_sock.sendall(METHOD_NOT_ALLOWED_RESPONSE)
+				else:
+					path = request['path']
+					if path == "/":
+						path = "/index.html"
+					filename = SERVER_ROOT + path
+
+					if os.path.isfile(filename):
+						file = open(filename, 'rb')
+						response_headers = FILE_HEADERS.format(content_type="text/html", content_length=os.path.getsize(filename)).encode('ASCII')
+						client_sock.sendall(response_headers)
+						client_sock.sendfile(file)
+					else:
+						client_sock.sendall(NOT_FOUND_RESPONSE)
+					client_sock.close()
 			except Exception as e:
 				print(f'Failed to parse request: {e}')
 				client_sock.sendall(BAD_REQUEST_RESPONSE)
